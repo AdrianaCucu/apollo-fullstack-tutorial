0726608ded75983338dfb170b6f2862a3ef4d593
Adriana <adrianacucu99@gmail.com>
2019-08-20 17:33:29 +0100

    Added Mutation type

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,5 +1,11 @@
 import { gql } from 'apollo-server';
 
+/**
+ * GraphQL cheatsheet:
+ *
+ * https://devhints.io/graphql#schema
+ */
+
 const typeDefs = gql`
   type Query {
     # Queries for the launches
@@ -8,6 +14,18 @@ const typeDefs = gql`
 
     # Queries for the current user
     me: User
+  }
+
+  # The Mutation type is the entry point into the graph for modifying data
+  type Mutation {
+    # if false, booking trips failed -> check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -> check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    # login token
+    login(email: String): String
   }
 
   type Launch {
@@ -19,25 +37,31 @@ const typeDefs = gql`
   }
 
   type Rocket {
-      id: ID!
-      name: String
-      type: String
+    id: ID!
+    name: String
+    type: String
   }
 
   type User {
-      id: ID!
-      email: String!
-      trips: [Launch]!
+    id: ID!
+    email: String!
+    trips: [Launch]!
   }
 
   type Mission {
-      name: String
-      missionPatch(size: PatchSize): String
+    name: String
+    missionPatch(size: PatchSize): String # GraphQL can contain arguments, not just queries
+  }
+
+  type TripUpdateResponse {
+      success: Boolean!
+      message: String
+      launches: [Launch]
   }
 
   enum PatchSize {
-      SMALL
-      LARGE
+    SMALL
+    LARGE
   }
 `;
 
