b485a840d00a42e96823e921dc8390569bc872fb
Adriana <adrianacucu99@gmail.com>
2019-08-21 09:14:02 +0100

    Created LaunchAPI; added reducer to fit the schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,6 +3,10 @@ const typeDefs = require('./schema');
 
 const server = new ApolloServer({ typeDefs });
 
+/**
+ * Server opens GraphQL playground by default.
+ * Can also see the schema from there.
+ */
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
