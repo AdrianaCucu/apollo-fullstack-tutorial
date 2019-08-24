2fde697b65e448d9b75ff54931930018a80c0011
Adriana <adrianacucu99@gmail.com>
2019-08-24 12:05:49 +0100

    Added login page (local query)

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,12 +1,15 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import gql from 'graphql-tag';
 
 import Pages from './pages';
+import Login from './pages/login';
 import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
 
 const cache = new InMemoryCache();
 
@@ -29,9 +32,22 @@ cache.writeData({
   }
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById('root')
 );
