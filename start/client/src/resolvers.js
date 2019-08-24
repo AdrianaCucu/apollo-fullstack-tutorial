b68752d44e90b454a65639110e530b174431e651
Adriana <adrianacucu99@gmail.com>
2019-08-24 12:44:40 +0100

    Extended Launch type in client schema

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+import { GET_CART_ITEMS } from './pages/cart';
+
 /**
  * To build a local schema, we *extend* the types of the
  * server schema and wrap it with the gql function.
@@ -23,4 +25,17 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
+
+export const resolvers = {
+  Launch: {
+    isInCart: (launch, _, { cache }) => {
+      const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+      return cartItems.includes(launch.id);
+    }
+  }
+};
